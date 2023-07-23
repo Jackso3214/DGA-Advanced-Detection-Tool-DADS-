6203ae2f404a570f8f68f3759e3fa981f884dd33
Orangexboom <78184505+Orangexboom@users.noreply.github.com>
2023-07-23 15:42:57 -0400

    added model save feature

--- Python Files/main.py
+++ Python Files/main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import DataPrep as prep
 import LogisticRegressionModel as logreg
+import saveModel as sm
 
 #prepare the data and output it to a csv called outputTable750k.csv
 #this is the main script for the program, run this if you want to test and run the program
@@ -9,6 +10,7 @@ prep.prepData()
 
 dataset = pd.read_csv("CSVs\output\outputTable10k_set1.csv")
 trainedModel = logreg.logRegModel(dataset)
+sm.savePickle(trainedModel)
 
 testing = prep.prepDataTest("3216549865165.test")
 result = trainedModel.predict(testing)
---
+++ Python Files/saveModel.py
@@ -0,0 +1,11 @@
+import pickle
+
+def savePickle(trainedModel):
+    
+    data = trainedModel
+    path = "saveModel\\saved_object.pkl"
+    file_name = 'saved_object.pkl'
+    with open(path, 'wb') as file:
+        pickle.dump(data, file)
+        file.close()
+    
